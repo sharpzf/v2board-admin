3657b32503dbdb88610b9e11fd338eb5693eb91d
sharp <2423550953@qq.com>
2024-11-11 20:22:19 +0800

    完善搜索

--- vendor/swiftmailer/swiftmailer/lib/classes/Swift/Transport/StreamBuffer.php
+++ vendor/swiftmailer/swiftmailer/lib/classes/Swift/Transport/StreamBuffer.php
@@ -263,7 +263,7 @@ class Swift_Transport_StreamBuffer extends Swift_ByteStream_AbstractFilterableIn
             throw new Swift_TransportException('Connection could not be established with host '.$this->params['host'].' :'.$msg);
         });
         try {
-            $this->stream = @stream_socket_client($host.':'.$this->params['port'], $errno, $errstr, $timeout, STREAM_CLIENT_CONNECT, $streamContext);
+            $this->stream = stream_socket_client($host.':'.$this->params['port'], $errno, $errstr, $timeout, STREAM_CLIENT_CONNECT, $streamContext);
         } finally {
             restore_error_handler();
         }
