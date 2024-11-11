13cb9c3f51a1634bcc7c9712e6da20093eccfe0f
js23 <348767710@qq.com>
2024-11-11 18:16:49 +0800

    123444444

--- app/Http/Controllers/Client/Protocols/Shadowrocket.php
+++ app/Http/Controllers/Client/Protocols/Shadowrocket.php
@@ -120,14 +120,58 @@ class Shadowrocket
         return $uri;
     }
 
-    public static function buildTrojan($password, $server)
+    public static function buildTrojan($uuid, $server)
     {
-        $name = rawurlencode($server['name']);
-        $query = http_build_query([
-            'allowInsecure' => $server['allow_insecure'],
-            'peer' => $server['server_name']
-        ]);
-        $uri = "trojan://{$password}@{$server['host']}:{$server['port']}?{$query}&tfo=1#{$name}";
+        $userinfo = base64_encode('auto:' . $uuid . '@' . $server['host'] . ':' . $server['port']);
+        $config = [
+            'tfo' => 1,
+            'remark' => $server['name'],
+            'alterId' => 0
+        ];
+        if ($server['tls']) {
+            $config['tls'] = 1;
+            if ($server['tlsSettings']) {
+                $tlsSettings = $server['tlsSettings'];
+                if (isset($tlsSettings['allowInsecure']) && !empty($tlsSettings['allowInsecure']))
+                    $config['allowInsecure'] = (int)$tlsSettings['allowInsecure'];
+                if (isset($tlsSettings['serverName']) && !empty($tlsSettings['serverName']))
+                    $config['peer'] = $tlsSettings['serverName'];
+            }
+        }
+        if ($server['network'] === 'tcp') {
+            if ($server['networkSettings']) {
+                $tcpSettings = $server['networkSettings'];
+                if (isset($tcpSettings['header']['type']) && !empty($tcpSettings['header']['type']))
+                    $config['obfs'] = $tcpSettings['header']['type'];
+                if (isset($tcpSettings['header']['request']['path'][0]) && !empty($tcpSettings['header']['request']['path'][0]))
+                    $config['path'] = $tcpSettings['header']['request']['path'][0];
+            }
+        }
+        if ($server['network'] === 'ws') {
+            $config['obfs'] = "websocket";
+            if ($server['networkSettings']) {
+                $wsSettings = $server['networkSettings'];
+                if (isset($wsSettings['path']) && !empty($wsSettings['path']))
+                    $config['path'] = $wsSettings['path'];
+                if (isset($wsSettings['headers']['Host']) && !empty($wsSettings['headers']['Host']))
+                    $config['obfsParam'] = $wsSettings['headers']['Host'];
+            }
+        }
+        if ($server['network'] === 'grpc') {
+            $config['obfs'] = "grpc";
+            if ($server['networkSettings']) {
+                $grpcSettings = $server['networkSettings'];
+                if (isset($grpcSettings['serviceName']) && !empty($grpcSettings['serviceName']))
+                    $config['path'] = $grpcSettings['serviceName'];
+            }
+            if (isset($tlsSettings)) {
+                $config['host'] = $tlsSettings['serverName'];
+            } else {
+                $config['host'] = $server['host'];
+            }
+        }
+        $query = http_build_query($config, '', '&', PHP_QUERY_RFC3986);
+        $uri = "vmess://{$userinfo}?{$query}";
         $uri .= "\r\n";
         return $uri;
     }
